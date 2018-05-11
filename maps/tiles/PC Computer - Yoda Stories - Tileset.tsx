48446f71430267aaba9e3a6cc4f066e119b660ca
Mark Hudnall <me@markhudnall.com>
2018-05-11 00:18:43 -0700

    Organize maps, tiles and sprites

---
+++ maps/tiles/PC Computer - Yoda Stories - Tileset.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="PC Computer - Yoda Stories - Tileset" tilewidth="32" tileheight="32" tilecount="1204" columns="28">
+ <image source="PC Computer - Yoda Stories - Tileset.png" trans="00ff00" width="896" height="1385"/>
+ <tile id="168">
+  <objectgroup draworder="index">
+   <object id="2" x="4" y="3">
+    <polygon points="0,0 -2,15 28,26 28,-4 1,-3"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="169">
+  <objectgroup draworder="index">
+   <object id="2" x="31" y="1">
+    <polygon points="0,0 -13,23 -33,28 -33,-3 -1,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
--- maps/tiles/hyptosis_sprites-and-tiles-for-you.tsx
+++ maps/tiles/hyptosis_sprites-and-tiles-for-you.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="hyptosis_sprites-and-tiles-for-you" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="../../../Downloads/hyptosis_sprites-and-tiles-for-you.png" width="960" height="960"/>
+ <image source="hyptosis_sprites-and-tiles-for-you.png" width="960" height="960"/>
 </tileset>
