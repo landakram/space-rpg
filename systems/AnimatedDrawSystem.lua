0594caae2766864f0a963df938d8a393a7e81460
Mark Hudnall <me@markhudnall.com>
2020-03-26 23:36:44 -0700

    Fix target drawing order
    The problem was that the indexes are actually target IDs, so the array is associative
    rather than indexed.

--- systems/AnimatedDrawSystem.lua
+++ systems/AnimatedDrawSystem.lua
@@ -7,14 +7,24 @@ function AnimatedDrawSystem:requires()
    return {"position", "animations"}
 end
 
+function AnimatedDrawSystem:sortedTargets()
+   local targets = {}
+   for _, t in pairs(self.targets) do
+      lume.push(targets, t)
+   end
+
+   table.sort(
+      targets,
+      function(a, b)
+         return a:get("position").y < b:get("position").y
+      end
+   )
+
+   return targets
+end
+
 function AnimatedDrawSystem:draw()
-   -- TODO this doesn't work
-   -- table.sort(
-   --    self.targets,
-   --    function(a, b) return a:get("position").y < b:get("position").y end
-   -- )
-   
-   for _, entity in pairs(self.targets) do
+   for key, entity in ipairs(self:sortedTargets()) do
       local position = entity:get("position")
       local animations = entity:get("animations")
 
